52d5d0418bc06db2b3ea85c5e10dccce92b0a06e
josembergsduarte@gmail.com <Js@72446066>
2023-06-18 18:33:21 -0300

    Limpesa do código

--- AppClienteVIP/app/src/main/java/br/com/dnsistemas/appclientevip/view/MainActivity.java
+++ AppClienteVIP/app/src/main/java/br/com/dnsistemas/appclientevip/view/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editText_primeiroNome;
     EditText editText_sobrenome;
     EditText editText_nomeCurso;
-    EditText editTExt_telefoneContato;
+    EditText editText_telefoneContato;
 
     Button button_limpar;
     Button button_salvar;
@@ -33,16 +33,17 @@ public class MainActivity extends AppCompatActivity {
         //Mock de Pessoa
         pessoa = new Pessoa("Josemberg", "Sousa Duarte", "Android", "86-99920-8114");
         initComponents();
-
         enviaDadosParaTela();
 
         button_limpar.setOnClickListener(v -> clearFields());
 
+
         button_salvar.setOnClickListener(v -> {
+            Pessoa pessoa = new Pessoa();
             pessoa.setPrimeiroNome(editText_primeiroNome.getText().toString());
             pessoa.setSobrenome(editText_sobrenome.getText().toString());
             pessoa.setCursoDesejado(editText_nomeCurso.getText().toString());
-            pessoa.setTelefoneContato(editTExt_telefoneContato.getText().toString());
+            pessoa.setTelefoneContato(editText_telefoneContato.getText().toString());
 
             Toast.makeText(MainActivity.this, pessoa.toString(), Toast.LENGTH_SHORT).show();
         });
@@ -53,24 +54,24 @@ public class MainActivity extends AppCompatActivity {
         editText_primeiroNome.setText("");
         editText_sobrenome.setText("");
         editText_nomeCurso.setText("");
-        editTExt_telefoneContato.setText("");
+        editText_telefoneContato.setText("");
     }
 
     private void enviaDadosParaTela() {
         editText_primeiroNome.setText(pessoa.getPrimeiroNome());
         editText_sobrenome.setText(pessoa.getSobrenome());
         editText_nomeCurso.setText(pessoa.getCursoDesejado());
-        editTExt_telefoneContato.setText(pessoa.getTelefoneContato());
+        editText_telefoneContato.setText(pessoa.getTelefoneContato());
     }
 
     private void initComponents() {
         editText_primeiroNome = findViewById(R.id.editText_firstName);
         editText_sobrenome = findViewById(R.id.editText_secondName);
         editText_nomeCurso = findViewById(R.id.editText_nomeCurso);
-        editTExt_telefoneContato = findViewById(R.id.editText_telefoneContato);
+        editText_telefoneContato = findViewById(R.id.editText_telefoneContato);
 
         button_limpar = findViewById(R.id.button_limpar);
-        button_limpar = findViewById(R.id.button_limpar);
+        button_salvar = findViewById(R.id.button_salvar);
         button_finalizar = findViewById(R.id.button_finalizar);
     }
 
