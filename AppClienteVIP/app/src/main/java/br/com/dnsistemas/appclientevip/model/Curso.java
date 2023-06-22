f99de48630ba0642b571cd7d21801216d7249c6f
josembergsduarte@gmail.com <Js@72446066>
2023-06-22 08:45:24 -0300

    Criação de método com Adapter para popular Spinner com Nomes dos Cursos

--- AppClienteVIP/app/src/main/java/br/com/dnsistemas/appclientevip/model/Curso.java
+++ AppClienteVIP/app/src/main/java/br/com/dnsistemas/appclientevip/model/Curso.java
@@ -5,11 +5,10 @@ import java.util.Date;
 public class Curso {
 
     private String nomeCurso;
-    private String descricao;
-    private Date dataInicio;
-    private Date dataFim;
-    private Integer cargaHorária;
-    private String professor;
+
+    public Curso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
 
     public String getNomeCurso() {
         return nomeCurso;
@@ -19,43 +18,4 @@ public class Curso {
         this.nomeCurso = nomeCurso;
     }
 
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Date getDataInicio() {
-        return dataInicio;
-    }
-
-    public void setDataInicio(Date dataInicio) {
-        this.dataInicio = dataInicio;
-    }
-
-    public Date getDataFim() {
-        return dataFim;
-    }
-
-    public void setDataFim(Date dataFim) {
-        this.dataFim = dataFim;
-    }
-
-    public Integer getCargaHorária() {
-        return cargaHorária;
-    }
-
-    public void setCargaHorária(Integer cargaHorária) {
-        this.cargaHorária = cargaHorária;
-    }
-
-    public String getProfessor() {
-        return professor;
-    }
-
-    public void setProfessor(String professor) {
-        this.professor = professor;
-    }
 }
--- AppClienteVIP/app/src/main/java/br/com/dnsistemas/appclientevip/view/MainActivity.java
+++ AppClienteVIP/app/src/main/java/br/com/dnsistemas/appclientevip/view/MainActivity.java
@@ -2,44 +2,52 @@ package br.com.dnsistemas.appclientevip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.List;
+
 import br.com.dnsistemas.appclientevip.R;
+import br.com.dnsistemas.appclientevip.controller.CursoController;
 import br.com.dnsistemas.appclientevip.controller.PessoaController;
 import br.com.dnsistemas.appclientevip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    List<String> nomeDosCursos;
+    CursoController cursoController;
     PessoaController controller;
-
-
     EditText editText_primeiroNome;
     EditText editText_sobrenome;
     EditText editText_nomeCurso;
     EditText editText_telefoneContato;
-
     Button button_limpar;
     Button button_salvar;
     Button button_finalizar;
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         controller = new PessoaController(MainActivity.this);
+        cursoController = new CursoController();
+        nomeDosCursos = cursoController.dadosParaSpinner();
 
         pessoa = new Pessoa();
+
         controller.buscar(pessoa);
 
         initComponents();
         enviaDadosParaTela();
+        populaAdapter();
 
         button_limpar.setOnClickListener(v -> {
             editText_primeiroNome.setText("");
@@ -48,10 +56,13 @@ public class MainActivity extends AppCompatActivity {
             editText_telefoneContato.setText("");
 
             controller.limpar();
+
         });
 
         button_salvar.setOnClickListener(v -> {
+
             Pessoa pessoa = new Pessoa();
+
             pessoa.setPrimeiroNome(editText_primeiroNome.getText().toString());
             pessoa.setSobrenome(editText_sobrenome.getText().toString());
             pessoa.setCursoDesejado(editText_nomeCurso.getText().toString());
@@ -63,6 +74,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         button_finalizar.setOnClickListener(v -> finish());
+    }
+
+    private void populaAdapter() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cursoController.dadosParaSpinner());
+        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        spinner.setAdapter(adapter);
     }
 
     private void enviaDadosParaTela() {
@@ -81,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
         button_limpar = findViewById(R.id.button_limpar);
         button_salvar = findViewById(R.id.button_salvar);
         button_finalizar = findViewById(R.id.button_finalizar);
+
+        spinner = findViewById(R.id.spinner_lista_cursos);
     }
 
 }
